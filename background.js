19cc25710a0ac89356afdbbbacfdea7a2f77f064
malem <malem.link@gmail.com>
2020-04-24 21:46:44 +0530

    background refactored

--- background.js
+++ background.js
@@ -23,104 +23,95 @@ let setOfTabs = new Set();
 let exclusionSet = new Set();
 let doubleClickPressedOnce = false;
 
-// chrome.browserAction.setBadgeBackgroundColor({
-//     color: [47, 47, 47, 255]
-// });
+const title_text = "Play-One  [Plays only one video/audio at a time]";
 
 function handleContentMessage(status, sender) {
 
-    if (status.mediaStatus === MEDIAEVENT.played) {
+    switch(status.mediaStatus){
+        case MEDIAEVENT.played:
 
-        console.log("playingTabId "+playingTabId);
-        // 1. media started playing in unfocused tab
-        if (sender.tab.id !== focusedTabId){
-            
-            chrome.tabs.sendMessage(sender.tab.id, {action: ACTION.pause});
-
-            setOfTabs.add(sender.tab.id);
-            // chrome.browserAction.setBadgeText({
-            //     text: "" + setOfTabs.size
-            // });
-        }
-        // 2. media started playing in now focused tab
-        else {
-
-            // 2.1 pause if there is an already playing media in another tab
-            if(sender.tab.id !== playingTabId && playingTabId){
-                chrome.tabs.sendMessage(playingTabId, {action: ACTION.pause});
+            console.log("playingTabId "+playingTabId);
+            // 1. media started playing in unfocused tab
+            if (sender.tab.id !== focusedTabId){
+                
+                chrome.tabs.sendMessage(sender.tab.id, {action: ACTION.pause});
+    
+                setOfTabs.add(sender.tab.id);
             }
-            console.log("1 playing tab id: "+playingTabId);
-            playingTabId = sender.tab.id;
-            console.log("2 playing tab id: "+playingTabId);
-
-            setOfTabs.add(playingTabId);
-            const site = getSite(sender.url)
-            updateIconTextOnEnabledSiteWithMedia(site);
-            // chrome.browserAction.setBadgeText({
-            //     text: "" + setOfTabs.size
-            // });
-        }
-
-    } else if (status.mediaStatus === MEDIAEVENT.ended) {
-
-        // remove the playingTabId if media has ended so that 2.1 will fail if media is replayed again in the same focused tab
-        if (sender.tab.id === playingTabId){
-            playingTabId = undefined;
-        }
-
-        setOfTabs.delete(sender.tab.id);
-
-        const site = getSite(sender.url);
-        updateIconTextOnEnabledSiteWithNoMedia(site);
-        // if(setOfTabs.size>0){
-        //     chrome.browserAction.setBadgeText({
-        //         text: "" + setOfTabs.size
-        //     });
-        // }else {
-        //     chrome.browserAction.setBadgeText({
-        //         text: ""
-        //     });
-        // }
-
-    } else if (status.mediaStatus === "am_i_focused") {
-
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            let focused = false;
-            if(tabs){
-                if(tabs[0].id === sender.tab.id) {
-                    focused = true;
-                }
-            }
-            chrome.tabs.sendMessage(sender.tab.id, {action: "am_i_focused", focused: focused});
-        });
-
-    } else if (status.mediaStatus === "continue_handle_on_tab_activated"){
-        // skip if focused tab is immediately changed before getting the userPaused response
-        if(sender.tab.id === focusedTabId){
-            // if focused tab has known to play media AND it is not the currently playing tab AND it was not paused by user
-            if (setOfTabs.has(focusedTabId) && focusedTabId !== playingTabId && !status.userPaused) {
-                if (playingTabId) {
+            // 2. media started playing in now focused tab
+            else {
+    
+                // 2.1 pause if there is an already playing media in another tab
+                if(sender.tab.id !== playingTabId && playingTabId){
                     chrome.tabs.sendMessage(playingTabId, {action: ACTION.pause});
-                    // console.log("sending pause");
                 }
-        
-                // playingTabId is now focusedTabId; 2.1 should not execute on played message from content script because of the consequence of below play message
-                playingTabId = focusedTabId;
-                chrome.tabs.sendMessage(playingTabId, {action: ACTION.play});
+                console.log("1 playing tab id: "+playingTabId);
+                playingTabId = sender.tab.id;
+                console.log("2 playing tab id: "+playingTabId);
+    
+                setOfTabs.add(playingTabId);
+                const site = getSite(sender.url)
+                updateIconTextOnEnabledSiteWithMedia(site);
             }
-        }
-    } else if (status.mediaStatus === "check_site_exclusion"){
-        console.log(sender.tab.url);
-        let site = getSite(sender.tab.url);
-        console.log(site);
-        let excluded = false;
-        if (exclusionSet.has(site)){
-            excluded = true;
-        }
-        chrome.tabs.sendMessage(sender.tab.id, {action: "check_site_exclusion", exclusion: excluded});
+            break;
+
+        case MEDIAEVENT.ended:
+
+            // remove the playingTabId if media has ended so that 2.1 will fail if media is replayed again in the same focused tab
+            if (sender.tab.id === playingTabId){
+                playingTabId = undefined;
+            }
+
+            setOfTabs.delete(sender.tab.id);
+
+            const site = getSite(sender.url);
+            updateIconTextOnEnabledSiteWithNoMedia(site);
+            break;
+
+        case "am_i_focused":
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                let focused = false;
+                if(tabs){
+                    if(tabs[0].id === sender.tab.id) {
+                        focused = true;
+                    }
+                }
+                chrome.tabs.sendMessage(sender.tab.id, {action: "am_i_focused", focused: focused});
+            });
+            break;
+
+        case "continue_handle_on_tab_activated":
+
+            // skip if focused tab is immediately changed before getting the userPaused response
+            if(sender.tab.id === focusedTabId){
+                // if focused tab has known to play media AND it is not the currently playing tab AND it was not paused by user
+                if (setOfTabs.has(focusedTabId) && focusedTabId !== playingTabId && !status.userPaused) {
+                    if (playingTabId) {
+                        chrome.tabs.sendMessage(playingTabId, {action: ACTION.pause});
+                        // console.log("sending pause");
+                    }
+            
+                    // playingTabId is now focusedTabId; 2.1 should not execute on played message from content script because of the consequence of below play message
+                    playingTabId = focusedTabId;
+                    chrome.tabs.sendMessage(playingTabId, {action: ACTION.play});
+                }
+            }
+            break;
+
+        case "check_site_exclusion":
+
+            console.log(sender.tab.url);
+            let site = getSite(sender.tab.url);
+            console.log(site);
+            let excluded = false;
+            if (exclusionSet.has(site)){
+                excluded = true;
+            }
+            chrome.tabs.sendMessage(sender.tab.id, {action: "check_site_exclusion", exclusion: excluded});
+            break;
+
     }
 }
-
 
 function handleOnTabActivated(tab) {
 
@@ -198,15 +189,6 @@ function forgetTab(tabId){
         playingTabId = undefined;
     }
     setOfTabs.delete(tabId);
-    // if(setOfTabs.size>0){
-    //     chrome.browserAction.setBadgeText({
-    //         text: "" + setOfTabs.size
-    //     });
-    // }else {
-    //     chrome.browserAction.setBadgeText({
-    //         text: ""
-    //     });
-    // }
 }
 
 function toggleExclusion(tab){
@@ -244,19 +226,18 @@ function includeOrExcludeSite(url, tabId){
 
 function updateIconTextOnEnabledSiteWithNoMedia(url){
     chrome.browserAction.setIcon({path : {"48": "images/icon_48_active_no_media.png"}});
-    chrome.browserAction.setTitle({title: "Double click to DISABLE on "+url+"\nPlay-One  [ Plays only one video/audio at a time ]"});
+    chrome.browserAction.setTitle({title: title_text+"\nDouble click to disable on "+url});
 }
 
 function updateIconTextOnEnabledSiteWithMedia(url){
     chrome.browserAction.setIcon({path : {"48": "images/icon_48_active_media.png"}});
-    chrome.browserAction.setTitle({title: "Double click to DISABLE on "+url+"\nPlay-One  [ Plays only one video/audio at a time ]"});
+    chrome.browserAction.setTitle({title: title_text+"\nDouble click to disable on "+url});
 }
 
 function updateIconTextOnDisabledSite(url){
     chrome.browserAction.setIcon({path : {"48": "images/icon_48_inactive.png"}});
-    chrome.browserAction.setTitle({title: "Double click to ENABLE on "+url+"\nPlay-One  [ Plays only one video/audio at a time ]"});
+    chrome.browserAction.setTitle({title: title_text+"\nDouble click to enable on "+url});
 }
-
 
 function getSite (a) {
     let b = a.indexOf("//") + 2;
